f2fb144ba7d5304df34d330979e0a2cfeba1d13a
savage <do_____bingo@outlook.com>
2021-09-18 14:08:12 +0800

    fix  去除多余导包

--- src/main/java/cn/github/savageyo/MybatisSensitiveApplication.java
+++ src/main/java/cn/github/savageyo/MybatisSensitiveApplication.java
@@ -1,7 +1,6 @@
 package cn.github.savageyo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
